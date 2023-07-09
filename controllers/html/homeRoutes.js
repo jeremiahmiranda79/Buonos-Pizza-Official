c9823bbebf56ed02e96bb0d95ed75e483b0dde73
Jeremiah Miranda <jeremiahmiranda79@gmail.com>
2023-07-09 13:20:24 -0700

    init

--- controllers/html/homeRoutes.js
+++ controllers/html/homeRoutes.js
@@ -42,41 +42,16 @@ router.get('/menu', async (req, res) => {
     };
 });
 
-router.get('/newmenu', async (req, res) => {
-    try {
-        const menu = await Categories.findAll({
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']//'id'
-            },
-            include: [
-                {model: MenuItems, attributes: {
-                    exclude: ['categoryId', 'modifierId', 'employeeId', 'createdAt', 'updatedAt', 'menuItemIds']//'id'
-                }},
-                {model: Modifiers, attributes: { 
-                    exclude: ['categoryId', 'notesForTheKitchen', 'createdAt', 'updatedAt' ]//'id'
-                }},
-                {model: Sizes}
-            ]
-        });
-        const serializedMenuitems = menu.map((menuitem) => menuitem.get({ plain: true }));
-        res.status(200).render('newmenu', {
-            category: serializedMenuitems
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error); // 500 - internal server error
-    };
-});
-
 // Router to get a menuitem by Id, to display single menu item, with mods
-router.get('/newmenu/:menuItemId', async (req, res) => {
+router.get('/menu/:menuItemId', async (req, res) => {
     try {
         const menuItem = await MenuItems.findByPk(req.params.menuItemId, {
             include: [
                 {model: Categories},
                 {model: Modifiers, attributes: { 
                     exclude: ['id', 'categoryId', 'notesForTheKitchen', 'createdAt', 'updatedAt']
-                }}
+                }},
+                {model: Sizes}
             ] 
         });
 
@@ -99,13 +74,79 @@ router.get('/newmenu/:menuItemId', async (req, res) => {
 
         res.status(200).render('product-quick-view', {
             layout: 'main',
-            item: result
+            item: result,
+            // size: 
         });
     } catch (error) {
         console.log(error);
         res.status(500).json(error); // 500 - internal server error
     };
 });
+
+// router.get('/newmenu', async (req, res) => {
+//     try {
+//         const menu = await Categories.findAll({
+//             attributes: {
+//                 exclude: ['createdAt', 'updatedAt']//'id'
+//             },
+//             include: [
+//                 {model: MenuItems, attributes: {
+//                     exclude: ['categoryId', 'modifierId', 'employeeId', 'createdAt', 'updatedAt', 'menuItemIds']//'id'
+//                 }},
+//                 {model: Modifiers, attributes: { 
+//                     exclude: ['categoryId', 'notesForTheKitchen', 'createdAt', 'updatedAt' ]//'id'
+//                 }},
+//                 {model: Sizes}
+//             ]
+//         });
+//         const serializedMenuitems = menu.map((menuitem) => menuitem.get({ plain: true }));
+//         res.status(200).render('newmenu', {
+//             category: serializedMenuitems
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json(error); // 500 - internal server error
+//     };
+// });
+
+// // Router to get a menuitem by Id, to display single menu item, with mods
+// router.get('/newmenu/:menuItemId', async (req, res) => {
+//     try {
+//         const menuItem = await MenuItems.findByPk(req.params.menuItemId, {
+//             include: [
+//                 {model: Categories},
+//                 {model: Modifiers, attributes: { 
+//                     exclude: ['id', 'categoryId', 'notesForTheKitchen', 'createdAt', 'updatedAt']
+//                 }}
+//             ] 
+//         });
+
+//         const serializedItem = menuItem.get({ plain: true });
+
+//         function removeNull(_serializedItem) {
+//             return Object.fromEntries(
+//               Object.entries(_serializedItem)
+//                 .filter(([_, value]) => value != null)
+//                 .map(([key, value]) => [
+//                   key,
+//                   value === Object(value) ? removeNull(value) : value,
+//                 ]),
+//             );
+//         }
+
+//         const result = removeNull(serializedItem)
+
+//         console.log(result);
+
+//         res.status(200).render('product-quick-view', {
+//             layout: 'main',
+//             item: result
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json(error); // 500 - internal server error
+//     };
+// });
 
 // Route gets all menu items by category with modifier attached to category instead of individual menu items
 router.get('/menuItemsMods', async (req, res) => {
